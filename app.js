03774c5b4695d0e381b904f2ea9d56408c4698d4
Nathan <nathaniellouie42@gmail.com>
2025-09-15 15:07:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const errors = require("celebrate");
 
 const { createUser, login } = require("./controllers/users");
 const mainRouter = require("./routes/index");
